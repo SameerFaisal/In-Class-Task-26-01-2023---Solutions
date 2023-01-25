d9eea1fc32535f2f9366269490cc44648067fd58
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-25 13:25:38 +0500

    26-01-2023 Task Solutions

---
+++ 26-01-2023 - Solutions/app.js
@@ -0,0 +1,45 @@
+//Task 1 - Aamna, Anas, Shahrukh, Hanzala
+// $(document).ready(function(){
+//     $("#change").click(function(){
+//         $("div").fadeToggle(5000);
+//     })
+
+// })
+
+//Task 2 - Arsalan, Hassan, Yusra
+// $(document).ready(function(){
+//     $("#show").click(function(){
+//         $("div").show(6000);
+//     })
+//     $("#hide").click(function(){
+//         $("div").hide(6000);
+//     })
+// })
+
+//Task 3 - Zain, Umer, Sadaf
+// $(document).ready(function(){
+//     $("#change").click(function(){
+//         $("img").animate({height: '300px'},2000);
+//         $("img").animate({width: '300px'},2000);
+//     })
+// })
+
+//Task 4 - Waseem, Muneeb, Sania
+// $(document).ready(function(){
+//     $("#change").click(function(){
+//         $("div").animate({fontSize: "20px"},"slow")
+//     })
+
+// })
+
+//Task 5 - Afnan, Asim
+// $(document).ready(function(){
+//     $("#tab").click(function(){
+//         let num=+prompt("Enter any number for table")
+// let i=1;
+//         while(i<=10){
+//             document.write(num+" * "+i+" = "+num*i)
+//             i++
+//         }
+//     })
+// })
